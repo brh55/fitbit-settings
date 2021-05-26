36f42022808f12ff5392d0ca7ea64b6b670efea7
Brandon Him <himbrandon1@gmail.com>
2021-05-26 14:35:48 -0400

    Add npmignore

---
+++ index.js
@@ -0,0 +1,55 @@
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+import { readFileSync, writeFileSync, existsSync } from 'fs';
+
+var _default = function _default(defaultSettings, filePath) {
+  var _this = this;
+
+  _classCallCheck(this, _default);
+
+  _defineProperty(this, "initial", {});
+
+  _defineProperty(this, "state", {});
+
+  _defineProperty(this, "filePath", 'settings.cbor');
+
+  _defineProperty(this, "save", function () {
+    writeFileSync(_this.filePath, _this.state, 'cbor');
+    return _this;
+  });
+
+  _defineProperty(this, "update", function (prop, value) {
+    _this.state[prop] = value;
+    return _this;
+  });
+
+  _defineProperty(this, "migrate", function (storedSettings, newSettings) {
+    var migratedSettings = Object.assign({}, newSettings, storedSettings);
+    _this.state = migratedSettings;
+  });
+
+  _defineProperty(this, "reset", function () {
+    _this.state = _this.initial;
+    return _this;
+  });
+
+  _defineProperty(this, "getProp", function (prop) {
+    return _this.state[prop];
+  });
+
+  this.initial = defaultSettings;
+  this.state = defaultSettings;
+  this.filePath = filePath || this.filePath; // Pre-Existing Users
+
+  if (existsSync(this.filePath)) {
+    var stored = readFileSync(this.filePath, 'cbor');
+    this.migrate(stored, this.initial);
+  }
+
+  this.save();
+};
+
+export { _default as default };
+;
