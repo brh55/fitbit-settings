a48dd5b05ce51e948378a220974071b357f0f6fa
Brandon Him <himbrandon1@gmail.com>
2021-05-26 00:08:23 -0400

    Initial commit v0.0.1

---
+++ index.test.js
@@ -0,0 +1,121 @@
+import Settings from '.';
+import fs from 'fs';
+
+jest.mock('fs');
+
+const mockStoredSettings = {
+    background: 'black',
+    name: 'henry',
+    border: 'red',
+    deprecatedProp: 'foo'
+};
+
+const mockWrite = fs.writeFileSync.mockReturnValue(true);
+
+describe('Fitbit Setting Module', () => {
+    test('initialize properly', () => {
+        fs.existsSync.mockReturnValue(true);
+        fs.readFileSync.mockReturnValue(mockStoredSettings);
+
+        const initialSettings = {
+            background: '#fff',
+            newProp: 'bar'
+        };
+        const settings = new Settings(initialSettings, 'file-path.cbor');
+
+        // Save to Disk Properly
+        expect(mockWrite.mock.calls[0][0]).toBe('file-path.cbor');
+        expect(mockWrite.mock.calls[0][1].name).toBe('henry'); // Check for Migration
+        expect(mockWrite.mock.calls[0][2]).toBe('cbor');
+
+        // Store initial settings
+        expect(settings.initial).toBe(initialSettings)
+
+        // Values Correct
+        expect(settings.getProp('newProp')).toBe('bar'); // Newly Added
+        expect(settings.getProp('name')).toBe('henry'); // Stored
+        expect(settings.getProp('deprecatedProp')).toBe('foo'); // Old Still Exists
+    });
+
+
+    test('update() reflects internal setting state', () => {
+        fs.existsSync.mockReturnValue(false);
+    
+        const settings = new Settings({
+            background: 'white'
+        });
+    
+        settings.update('background', 'yellow');
+        expect(settings.getProp('background')).toBe('yellow');
+    });
+    
+    test('stored settings persist new initial settings in state', () => {
+        fs.existsSync.mockReturnValue(true);
+        fs.readFileSync.mockReturnValue(mockStoredSettings);
+
+        const settings = new Settings({
+            background: 'black',
+            name: 'joe',
+            newProp: 'bar'
+        });
+
+        expect(settings.getProp('name')).toBe('henry');
+        expect(settings.getProp('border')).toBe('red');
+        expect(settings.getProp('background')).toBe('black');
+        expect(settings.getProp('deprecatedProp')).toBe('foo'); 
+    });
+
+        
+    test('return value with getProp()', () => {
+        const settings = new Settings({
+            test: 1
+        });
+
+        expect(settings.getProp('test')).toBe(1);
+    });
+
+    test('reset() clears internal state', () => {
+        const settings = new Settings({
+            prop1: 1,
+            prop2: 2,
+        });
+
+        // Update
+        settings.update('prop1', 2);
+        settings.update('prop2', 3);
+        expect(settings.getProp('prop1', 2));
+        expect(settings.getProp('prop1', 3));
+
+        // Return to normal
+        settings.reset();
+        expect(settings.getProp('prop1')).toBe(1);
+        expect(settings.getProp('prop2')).toBe(2);
+    });
+
+    test('save() stores state to disk', () => {
+        fs.existsSync.mockReturnValue(false);
+
+        const settings = new Settings({
+            foo: 'bar'
+        }, 'testpath.cbor');
+
+
+        // Update
+        settings.update('choo', 'choo');
+        expect(mockWrite).toHaveBeenLastCalledWith('testpath.cbor', {
+            foo: 'bar',
+            choo: 'choo'
+        }, 'cbor');
+    });
+
+    test('main methods should be chainable', () => {
+        const settings = new Settings({ 'foo': 'bar' });
+        settings.update('choo', 'choo').update('mary', 'lamb').save();
+        expect(settings.getProp('mary', 'lamb'))
+        expect(mockWrite).toHaveBeenLastCalledWith('settings.cbor', { 
+            foo: 'bar',
+            choo: 'choo',
+            mary: 'lamb'
+         }, 'cbor');
+    });
+});
---
+++ readme.md
@@ -0,0 +1,89 @@
+# fitbit-settings [![Travis branch](https://img.shields.io/travis/brh55/fitbit-settings/master.svg?style=flat-square)](https://travis-ci.org/brh55/fitbit-settings) [![Coveralls branch](https://img.shields.io/coveralls/brh55/fitbit-settings/master.svg?style=flat-square)](https://coveralls.io/github/brh55/fitbit-settings) [![npm badge](https://img.shields.io/npm/dt/fitbit-settings.svg?style=flat-square)](https://www.npmjs.com/package/fitbit-settings)
+
+> A dead simple module to assist managing user settings within Fitbit watch faces and applications
+
+`fitbit-settings` is designed to be a simple way to help manage essential aspects of user settings within your watch faces and applications. It includes some common goodies like persisting into disk, chainable methods for managing state, and migration handling. Given it's simple nature, it doesn't handle complex scenarios well such as nested properties, so be warned, *matey*.
+
+## Install
+
+```
+$ npm install --save fitbit-settings
+```
+
+## Usage
+
+```js
+import Settings from 'fitbit-settings';
+
+const appSettings = new Settings({
+    color_background: '#000',
+    color_label: '#fff',
+    hide_battery: false,
+    tap_disable: true,
+    color_hour: '#000cec9',
+    color_minute: '#fff',
+    color_battery: '#fff'
+});
+
+appSettings
+    .update('color_battery', 'red')
+    .update('color_hour', 'yellow')
+    .save(); // Update State and Persist to Disk
+
+appSettings.reset().save(); // Reset user settings
+document.getElementById('battery').style.fill = appSettings.getProp('color_battery');
+```
+
+## API
+
+### Settings()
+**Type:** `class`<br>
+**Usage:** `new Settings(defaultSettings, filePath)`
+
+#### defaultSettings
+**Type:** `object`<br>
+Default settings for the application / watch face
+
+#### filePath
+**Type:** `string`<br>
+Override the default file path ('settings.cbor').
+
+## Instance Methods
+### .getProp(propName)
+**Returns:** Value of prop stored in memory state
+
+#### propName
+**Type:** `string`<br>
+
+Prop of setting to retrieve `IE: 'color_battery'`
+
+### .update(prop, value)
+Returns class `instance`
+
+If no prop currently exists, this will create a new one.
+
+#### prop
+**Type:** `string`<br>
+
+#### value
+**Type:** `any`<br>
+
+### .save()
+Returns class `instance`
+
+Persist existing setting state to watch disk.
+
+### .reset()
+Returns class `instance`
+
+Reset internal setting state back to default settings
+
+## Used By
+`fitbit-settings` is currently being used in these production watch faces and applications:
+    - [Pixels on Ridge](https://pixelsonridge.com)
+
+> Feel free to submit your studio with a GitHub issue
+
+## License
+
+MIT © [Brandon Him](https://github.com/brh55/fitbit-settings)
